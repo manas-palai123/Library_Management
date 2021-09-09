5504eecb50c542fec35ea7ea483edc84e6459cf0
manas-palai123 <manas.palai2005@gmail.com>
2021-09-09 22:39:03 +0530

    Update urls.py

--- Library_management/urls.py
+++ Library_management/urls.py
@@ -20,6 +20,7 @@ from django.urls import path
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 from management import views
 from django.contrib.auth import views as auth_views
@@ -31,6 +32,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('/feed/', LatestEntriesFeed()),
+    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+
 
     path('books/', views.BookListView, name='books'),
     path('book/<int:pk>', views.BookDetailView, name='book-detail'),
